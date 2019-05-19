eafbfd6b9610b3918d844c09fb2a8f55038b8b67
micha <micha@132.73.202.141>
2019-05-19 20:09:39 +0300

    bloom filter constructor

--- Assignment 4/src/BloomFilter.java
+++ Assignment 4/src/BloomFilter.java
@@ -4,21 +4,40 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 public class BloomFilter {
-	private File Hashfunncshens=""
+	private String Hashfunncshens="hash_functions.txt";
+	private LinkedList<int[]> FuncshenList;
+    private byte[] set;
+    private int keySize, setSize, size;
 	
 	
-private void LoudHashFuncshens {
+public BloomFilter(int capacity,int k) {
+	
+	FuncshenList=new LinkedList<int[]>();
+    setSize = capacity;
+    set = new byte[setSize];
+    keySize = k;
+    size = 0;
+	
+	}
+	
+	
+private  void LoudHashFuncshens() {
+	String input;
 	try {
         // FileReader reads text files in the default encoding.
         FileReader fileReader = 
-            new FileReader(fileName);
+            new FileReader(Hashfunncshens);
 
         // Always wrap FileReader in BufferedReader.
         BufferedReader bufferedReader = 
             new BufferedReader(fileReader);
 
-        while((line = bufferedReader.readLine()) != null) {
-            System.out.println(line);
+        while((input=bufferedReader.readLine()) != null) {
+            String[] str=input.split("_");
+            int[] array=new int[2];
+            array[0]= Integer.parseInt(str[0]);
+            array[1]= Integer.parseInt(str[1]);
+            FuncshenList.add(array);
         }   
 
         // Always close files.
@@ -26,15 +45,11 @@ private void LoudHashFuncshens {
     }
     catch(FileNotFoundException ex) {
         System.out.println(
-            "Unable to open file '" + 
-            fileName + "'");                
+            "Unable to open file");                
     }
     catch(IOException ex) {
-        System.out.println(
-            "Error reading file '" 
-            + fileName + "'");                  
-        // Or we could just do this: 
-        // ex.printStackTrace();
+        System.out.println("Error reading file");                  
+
     }
 }
 }
