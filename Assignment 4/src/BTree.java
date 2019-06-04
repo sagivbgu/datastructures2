776ffba46b4a9ddcf563dbc1bb418a0e1df72d8c
sagivbgu <sagivsc@post.bgu.ac.il>
2019-06-05 00:10:35 +0300

    Implemented BTree delete

--- Assignment 4/src/BTree.java
+++ Assignment 4/src/BTree.java
@@ -144,7 +144,7 @@ public class BTree {
     }
 
     // Get the index of the first key in the node that is not greater than the key parameter,
-    // or node.getNumOfKeys() if not found
+    // or node.getNumOfKeys()-1 if not found
     private int indexOfNotGreater(BTreeNode node, String key) {
         int i = 0;
         while (i < node.getNumOfKeys() - 1 && key.compareTo(node.getValue(i)) > 0) {
@@ -178,18 +178,64 @@ public class BTree {
         int i = indexOfNotGreater(root, key);
 
         if (!key.equals(root.getValue(i))) {
-            BTreeNode nextNode = root.getChild(i);
-            if (nextNode != null) {
-                // TODO: Make sure the next node has at least t elements
-                delete(nextNode, key);
+            if (root.isLeaf) {
+                return;
             }
-        }
-        else {
+            if (key.compareTo(root.getValue(i)) > 0) {
+                i++;
+            }
+
+            BTreeNode child = prepareChildForDeletion(root, i);
+            delete(child, key);
+
+        } else {
             if (root.isLeaf) {
                 deleteKeyFromNode(root, i);
             }
-            // TODO
+
+            BTreeNode rightChild = root.getChild(i + 1);
+            BTreeNode leftChild = root.getChild(i);
+            if (leftChild.getNumOfKeys() > t - 1) {
+                String predecessor = getPredecessor(root, i);
+                delete(leftChild, predecessor);
+                root.setValue(i, predecessor);
+            } else if (rightChild.getNumOfKeys() > t - 1) {
+                String successor = getSuccessor(root, i);
+                delete(rightChild, successor);
+                root.setValue(i, successor);
+            } else {
+                BTreeNode mergedChild = merge(root, i);
+                delete(mergedChild, key);
+            }
         }
+    }
+
+    // Make sure the next node has at least t elements
+    private BTreeNode prepareChildForDeletion(BTreeNode parent, int childIndex) {
+        BTreeNode child = parent.getChild(childIndex);
+        BTreeNode prevChild = childIndex == 0 ? null : parent.getChild(childIndex - 1);
+        BTreeNode nextChild = childIndex == parent.getNumOfKeys() - 1 ? null : parent.getChild(childIndex + 1);
+        if (child.getNumOfKeys() >= t) {
+            return child;
+        }
+        if (prevChild != null && prevChild.getNumOfKeys() > t - 1) {
+            shiftRight(parent, prevChild, child, childIndex - 1);
+        } else if (nextChild != null && nextChild.getNumOfKeys() > t - 1) {
+            shiftLeft(parent, child, nextChild, childIndex);
+        } else {
+            child = merge(parent, childIndex);
+        }
+        return child;
+    }
+
+    private BTreeNode decideNodeToDeleteFrom(BTreeNode root, String key, int i) {
+        BTreeNode childNode;
+        if (key.compareTo(root.getValue(i)) > 0) {
+            childNode = root.getChild(i + 1);
+        } else {
+            childNode = root.getChild(i);
+        }
+        return childNode;
     }
 
     private void deleteKeyFromNode(BTreeNode node, int index) {
@@ -198,7 +244,7 @@ public class BTree {
         node.setNumOfKeys(node.getNumOfKeys() - 1);
     }
 
-    private void shiftRight(BTreeNode parent, BTreeNode left, BTreeNode right, int keyIndex) {
+    private void shiftRight(BTreeNode parent, BTreeNode left, BTreeNode right, int leftChildIndex) {
         String leftKeyToShift = left.getValue(left.getNumOfKeys() - 1);
         BTreeNode childToShift = left.getChild(left.getNumOfKeys());
 
@@ -206,12 +252,12 @@ public class BTree {
         shiftChildrenForward(right);
 
         // Add key and child to the right node
-        String parentKeyToShift = parent.getValue(keyIndex);
+        String parentKeyToShift = parent.getValue(leftChildIndex);
         right.setValue(0, parentKeyToShift);
         right.setChild(0, childToShift);
         right.setNumOfKeys(right.getNumOfKeys() + 1);
 
-        parent.setValue(keyIndex, leftKeyToShift);
+        parent.setValue(leftChildIndex, leftKeyToShift);
 
         deleteKeyFromNode(left, left.getNumOfKeys() - 1);
     }
@@ -228,17 +274,17 @@ public class BTree {
         }
     }
 
-    private void shiftLeft(BTreeNode parent, BTreeNode left, BTreeNode right, int keyIndex) {
+    private void shiftLeft(BTreeNode parent, BTreeNode left, BTreeNode right, int leftChildIndex) {
         String rightKeyToShift = right.getValue(0);
         BTreeNode childToShift = right.getChild(0);
 
         // Add key and child to the left node
-        String parentKeyToShift = parent.getValue(keyIndex);
+        String parentKeyToShift = parent.getValue(leftChildIndex);
         left.setValue(left.getNumOfKeys() - 1, parentKeyToShift);
         left.setChild(left.getNumOfKeys(), childToShift);
         left.setNumOfKeys(right.getNumOfKeys() + 1);
 
-        parent.setValue(keyIndex, rightKeyToShift);
+        parent.setValue(leftChildIndex, rightKeyToShift);
 
         deleteKeyFromNode(right, 0);
     }
